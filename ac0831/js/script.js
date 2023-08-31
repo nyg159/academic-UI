ab3a6040be1d56b8bcdab36169deff436e97477e
nyg159 <nyg159@gmail.com>
2023-08-31 13:59:28 +0900

    0831

---
+++ ac0831/js/script.js
@@ -0,0 +1,18 @@
+let li = document.getElementsByTagName('li');
+
+li[0].innerHTML = `test 입니다~`;
+
+let sum = 0;
+
+for(let i = 0; i <=5; i++){
+    sum += i;
+
+}
+
+li[1].innerHTML = `1 ~ 5 합계 : ${sum}`;
+
+
+
+
+
+
